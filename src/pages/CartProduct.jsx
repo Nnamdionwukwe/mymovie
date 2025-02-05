b19b61a8d651da43b474bcb392288e7f1f4f552f
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-02-05 14:09:37 +0100

    new

--- src/pages/CartProduct.jsx
+++ src/pages/CartProduct.jsx
@@ -8,6 +8,16 @@ const CartProduct = ({ product }) => {
       <div className="details">
         <p>{product.title}</p>
         <p>{product.price}</p>
+
+        <div className="buttons">
+          <button className="rounded-circle px-2">
+            <b>-</b>
+          </button>
+          <button className="rounded">{product.quantity}</button>
+          <button className="rounded-circle px-2">
+            <b>+</b>
+          </button>
+        </div>
       </div>
     </div>
   );
