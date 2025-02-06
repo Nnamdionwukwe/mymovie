5144535137c44ccfbc37a44006088df3389ff9ac
Nnamdi Onwukwe <nnamdionwuke@gmail.com>
2025-02-06 23:07:30 +0100

    new

--- src/Components/Features/ContextProvider.jsx
+++ src/Components/Features/ContextProvider.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/prop-types */
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import CartReduser from "./CartReduser";
 
 export const CartContext = createContext();
 
+const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]");
+
 const ContextProvider = ({ children }) => {
-  const [cart, dispatch] = useReducer(CartReduser, []);
+  function store() {
+    const storedValue = localStorage.getItem("cart");
+    return JSON.parse(storedValue);
+  }
+
+  const [cart, dispatch] = useReducer(CartReduser, store() || []);
+
+  useEffect(
+    function () {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    },
+    [cart]
+  );
 
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
